aeeda6557fda5b37b099f607b2c7d4163a1b86d0
Mathys-d <mathys.deronne@le-campus-numerique.fr>
2025-05-12 16:54:55 +0200

    add some new feat

--- site-test/scripts/scripts.js
+++ site-test/scripts/scripts.js
@@ -1,97 +1,94 @@
-feedPage = 1;
-const tab = {
-    strDrink:[],
-    strCategory:[],
-};
+let feedPage = 1;
 
+const tab = {
+    strDrink: [],
+    strCategory: [],
+    dose: [],
+    liste: [],
+    ingrd: [],
+    strInstructions: [],
+};
 
 let cpt = 0;
 let l = 0;
 let start = 0;
+
 function newArticle(response) {
-console.log("reponse",response.strDrink)
-    const article = document.createElement('article')
+    console.log("reponse", response.strDrink);
+    const article = document.createElement('article');
     article.innerHTML = `
-    <div class="feedBox" onclick="toggleInfo(this)">
-        <p>${response.strDrink}</p>
-        <p>${response.strCategory}</p> 
-        
-        <div class="feedBoxPlus">
-        ${[...Array(15).keys()].map(i => {
-        const ingredient = response[`strIngredient${i + 1}`];
-        const measure = response[`strMeasure${i + 1}`];
-        return ingredient ? `<p>${measure ? measure + " of " : ""}${ingredient}</p>` : "";
-    }).join("")}
-        ${response.strGlass ? `<p>Glass: ${response.strGlass}</p>` : ""}
-        ${response.strInstructions ? `<p>Instructions: ${response.strInstructions}</p>` : ""}
-        </div>
-    </div>`;
-    return article
+        <div class="feedBox" onclick="toggleInfo(this)">
+            <p>${response.strDrink}</p>
+            <p>${response.strCategory}</p> 
+            <div class="feedBoxPlus">
+                ${[...Array(15).keys()].map(i => {
+                    const ingredient = response[`strIngredient${i + 1}`];
+                    const measure = response[`strMeasure${i + 1}`];
+                    return ingredient ? `<p>${measure ? measure + " of " : ""}${ingredient}</p>` : "";
+                }).join("")}
+                ${response.strInstructions ? `<p>Instructions: ${response.strInstructions}</p>` : ""}
+            </div>
+        </div>`;
+    return article;
 }
 
- 
-const send = (event) => {
+function send(event) {
     event.preventDefault();
-    event.stopPropagation();
 
-    console.log('element clicked');
+    const drink = document.querySelector('#strDrink').value.trim();
+    const category = document.querySelector('#strCategory').value;
+    const instructions = document.querySelector('#strInstructions').value.trim();
+    const ingredient = document.querySelector('#ingrd').value.trim();
+    const unit = document.querySelector('#liste').value;
+    const quantity = document.querySelector('#dose').value;
 
-    let newDrinks = document.querySelector('#strDrink').value;
-    let newCategory = document.querySelector('#strCategory').value;
-   // let newInstructions = document.querySelector('#strInstructions').value;
-    let newIngredient1 = document.querySelector('#ingrd').value;
-
-    if (newDrinks,newCategory,/*newInstructions*/ ingrd) {
+    if (drink && category && instructions && ingredient && unit && quantity) {
+        const measure = `${quantity} ${unit}`;
         const wrapper = document.querySelector('#lastPosts');
-        wrapper.prepend(newArticle({"strDrink":newDrinks, "strCategory" :newCategory /*, "strInstructions" :newInstructions*/ , "ingrd":newIngredient1 }))
-        console.log("newDrinks : ", newDrinks);
+        wrapper.prepend(newArticle({
+            strDrink: drink,
+            strCategory: category,
+            strMeasure1: measure,
+            strIngredient1: ingredient,
+            strInstructions: instructions
+        }));
     }
 }
-
 
 const wait = (delay = 1000) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve()
-        }, delay)
-    })
-}
-
-//wait(3000).then(() => console.log("après mon délai"))
-
+            resolve();
+        }, delay);
+    });
+};
 
 async function main() {
-
     for (cpt = 0; cpt < feedPage; cpt++) {
-
-        const wrapper = document.querySelector('#lastPosts')
-        const loader = document.createElement('p')
-        loader.innerText = 'Chargement...'
-        wrapper.prepend(loader)
+        const wrapper = document.querySelector('#lastPosts');
+        const loader = document.createElement('p');
+        loader.innerText = 'Chargement...';
+        wrapper.prepend(loader);
 
         try {
             fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
                 .then(response => response.json())
                 .then(response => {
-                    // console.log(response)
-                    loader.remove()
-
-                    wrapper.prepend(newArticle(response.drinks[0]))
-                    tab.strDrink.push(response.drinks[0].strDrink)
-                    tab.strCategory.push(response.drinks[0].strCategory)
-                })
-        }
-        catch (e) {
-            loader.innerText = 'impossible a charger'
-            loader.style.color = 'red'
+                    loader.remove();
+                    wrapper.prepend(newArticle(response.drinks[0]));
+                    tab.strDrink.push(response.drinks[0].strDrink);
+                    tab.strCategory.push(response.drinks[0].strCategory);
+                });
+        } catch (e) {
+            loader.innerText = 'impossible a charger';
+            loader.style.color = 'red';
             break;
         }
-        await wait(500)
-        console.log("après mon délai")
+
+        await wait(500);
+        console.log("après mon délai");
     }
-
-}// console.log("erreur de chargement")
-
+}
 
 document.querySelector('#raf').addEventListener('click', function () {
     let allChildrens = document.querySelectorAll(".feedBox");
@@ -112,69 +109,67 @@ function toggleForm() {
         ouvrir.style.display = "none";
     } else {
         ouvrir.style.display = "block";
-      }    
-    console.log("adfadzdza")
+    }
 }
 
 main();
 
+let num = 1;
 
-function ajout(element){
-
+function ajout(element) {
     let formulaire = window.document.formulairedrks;
-       
-    let bloc1 = document.createElement("span");
-    let bloc2 = document.createElement("span");
-    let bloc3 = document.createElement("span");
 
+    let sup = document.createElement("button");
+    sup.type = "button";
+    sup.id = "btn";
+    sup.textContent = "X";
+    sup.onclick = function () {
+        suppression(groupe);
+    };
+
+    let groupe = document.createElement("div");
 
     let champ1 = document.createElement("input");
     champ1.name = "champs1";
     champ1.type = "text";
-    champ1.id = "ingrd";
+    champ1.id = "ingrd" + num;
 
     let champ2 = document.createElement("input");
     champ2.name = "champs2";
     champ2.type = "number";
-    champ2.id = "dose";
-    
-    let champ3 = document.createElement("input");
-    champ3.type ="list" 
-    champ3.list="champs3" 
-    champ3.id ="liste" 
-    champ3.name ="champs3"
+    champ2.id = "dose" + num;
+    champ2.min = "0";
+    champ2.value = "0";
 
+    let champ3 = document.createElement("select");
+    champ3.name = "champs3";
+    champ3.id = "liste" + num;
+    champ3.required = true;
 
-    let sup = document.createElement("input");
-    sup.value = "X";
-    sup.type = "button";
-    sup.id = "btn";  
+    const options = [
+        "cl", "oz", "shot", "cup", "cup pure",
+        "drops", "scoops", "splashes", "packages"
+    ];
 
+    options.forEach(value => {
+        let option = document.createElement("option");
+        option.value = value;
+        option.textContent = value;
+        champ3.appendChild(option);
+    });
 
-    
-    sup.onclick = function onclick(event)
-    {suppression(this);};
+    groupe.appendChild(champ2);
+    groupe.appendChild(champ3);
+    groupe.appendChild(champ1);
+    groupe.appendChild(sup);
+    groupe.appendChild(document.createElement("br"));
 
-    bloc2.appendChild(champ2);
-    bloc3.appendChild(champ3);
-    bloc1.appendChild(champ1);
-
-    element.after(bloc1);
-    element.after(bloc3);
-    element.after(bloc2);
-    element.after(sup); 
-
-    
-
-    let br = document.createElement("br");
-    element.after(br);
+    element.after(groupe);
+    num++;
 }
 
-
-function suppression(element){
+function suppression(element) {
     let formulaire = window.document.formulairedrks;
-    formulaire.removeChild(element.nextSibling);
-    formulaire.removeChild(element.nextSibling);
-    formulaire.removeChild(element.nextSibling);
-    formulaire.removeChild(element);
+    element.remove();
+    num--;
 }
